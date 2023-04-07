1203035cfece1bffd8e6bc787ebe309f40e401f0
Kanayo <anayoifediora@live.com>
2023-04-07 23:16:02 +0930

    completed HTML and CSS code

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+var startButton = document.querySelector(".start-button");
+var questionSection = document.querySelector(".question-section");
+/*var questions = "";
+var options = [];
+var correctOption = "";*/
+
+
+startButton.addEventListener("click", function () {
+    questionSection.classList.add('show');
+});
+
+
+
