2ca27916afb7cdcd47c70fe2224546533ce7da9f
Kanayo <anayoifediora@live.com>
2023-04-09 19:20:12 +0930

    Queried the DOM to create and append elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,63 @@
-var startButton = document.querySelector(".start-button");
+var body = document.body;
+var startButton = document.getElementById("#start-button");
 var questionSection = document.querySelector(".question-section");
-/*var questions = "";
+
+var questionEl = document.createElement('p');
+var divQuestionsEl = document.createElement('div');
+var divAnswerEl = document.createElement('div');
+var olEl = document.createElement('ol');
+var answerButton1 = document.createElement('button');
+var answerButton2 = document.createElement('button');
+var answerButton3 = document.createElement('button');
+var answerButton4 = document.createElement('button');
+var liEl1 = document.createElement('li');
+var liEl2 = document.createElement('li');
+var liEl3 = document.createElement('li');
+var liEl4 = document.createElement('li');
+
+divQuestionsEl.setAttribute('class', 'questions');
+divAnswerEl.setAttribute('class', 'answer-buttons');
+
+questionEl.textContent = "Commonly used data types do not include:";
+answerButton1.textContent = "Strings";
+answerButton2.textContent = "Boolean";
+answerButton3.textContent = "Alert";
+answerButton4.textContent = "Numbers";
+
+questionSection.appendChild(divQuestionsEl);
+divQuestionsEl.appendChild(questionEl);
+questionSection.appendChild(divAnswerEl);
+divAnswerEl.appendChild(olEl);
+olEl.appendChild(liEl1);
+olEl.appendChild(liEl2);
+olEl.appendChild(liEl3);
+olEl.appendChild(liEl4);
+
+liEl1.appendChild(answerButton1);
+liEl2.appendChild(answerButton2);
+liEl3.appendChild(answerButton3);
+liEl4.appendChild(answerButton4);
+
+
+
+
 var options = [];
-var correctOption = "";*/
+var correctOption = ""
 
+/* function startQuiz () {
+    renderQuestions();
 
-startButton.addEventListener("click", function () {
-    questionSection.classList.add('show');
-});
+}; */
 
+function renderQuestions() {
 
+    document.body.questionSection;    
+    /* var questionParagraph = document.createElement('p');
+    questionParagraph.textContent = "Commonly used data types do not include:";
+    questionSection.appendChild(questionParagraph); */
+};
+
+renderQuestions();
+
+//startButton.addEventListener("click", startQuiz);
 
