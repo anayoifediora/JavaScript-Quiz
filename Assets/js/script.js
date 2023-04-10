4a942a36f658fa322254768e27368a2bb46e4957
Kanayo <anayoifediora@live.com>
2023-04-10 23:49:55 +0930

    Added conditional statements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 var body = document.body;
-var startButton = document.getElementById("#start-button");
+var startButton = document.querySelector("#start-button");
 var questionSection = document.querySelector(".question-section");
 
 var questionEl = document.createElement('p');
@@ -14,9 +14,13 @@ var liEl1 = document.createElement('li');
 var liEl2 = document.createElement('li');
 var liEl3 = document.createElement('li');
 var liEl4 = document.createElement('li');
+var answerCommentEl = document.createElement('p');
 
 divQuestionsEl.setAttribute('class', 'questions');
 divAnswerEl.setAttribute('class', 'answer-buttons');
+
+
+function renderQuestion1() {
 
 questionEl.textContent = "Commonly used data types do not include:";
 answerButton1.textContent = "Strings";
@@ -37,27 +41,98 @@ liEl1.appendChild(answerButton1);
 liEl2.appendChild(answerButton2);
 liEl3.appendChild(answerButton3);
 liEl4.appendChild(answerButton4);
-
-
-
-
-var options = [];
-var correctOption = ""
-
-/* function startQuiz () {
-    renderQuestions();
-
-}; */
-
-function renderQuestions() {
-
-    document.body.questionSection;    
-    /* var questionParagraph = document.createElement('p');
-    questionParagraph.textContent = "Commonly used data types do not include:";
-    questionSection.appendChild(questionParagraph); */
 };
 
-renderQuestions();
+function renderQuestion2() {
 
-//startButton.addEventListener("click", startQuiz);
+    questionEl.textContent = "The condition in an if/else statement is enclosed within __________";
+    answerButton1.textContent = "Quotes";
+    answerButton2.textContent = "Parentheses";
+    answerButton3.textContent = "Curly brackets";
+    answerButton4.textContent = "Square Brackets";
+    
+    
+};
 
+function renderQuestion3() {
+    
+        questionEl.textContent = "Arrays in JavaScript can be used to store";
+        answerButton1.textContent = "Numbers and Strings";
+        answerButton2.textContent = "Other arrays";
+        answerButton3.textContent = "Boolean";
+        answerButton4.textContent = "All of the above";
+        
+};
+
+function renderQuestion4() {
+    
+    questionEl.textContent = "String values must be enclosed within ____________ when being assigned to variables";
+    answerButton1.textContent = "Commas";
+    answerButton2.textContent = "Curly brackets";
+    answerButton3.textContent = "Quotes";
+    answerButton4.textContent = "Parentheses";
+    
+};
+
+function renderQuestion5() {
+    
+    questionEl.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
+    answerButton1.textContent = "Console.log";
+    answerButton2.textContent = "For loops";
+    answerButton3.textContent = "Terminal/bash";
+    answerButton4.textContent = "JavaScript";
+    
+};
+
+
+
+
+startButton.addEventListener("click", renderQuestion1);
+
+var correctOption = answerButton3;
+var wrongOptions = [answerButton1, answerButton2, answerButton4];
+
+if (correctOption) {
+
+    correctOption.addEventListener('click', function () {
+        answerCommentEl.textContent = alert("Correct!");
+        answerCommentEl.setAttribute('style', 'font-size: 30px; position: relative; left: 400px');
+        body.appendChild(answerCommentEl);
+        renderQuestion2();
+    });
+} 
+if (wrongOptions) {
+       
+        for (var i = 0; i < wrongOptions.length; i++) {
+        wrongOptions[i].addEventListener('click', function () {
+        answerCommentEl.textContent = alert("Wrong ❌");
+        answerCommentEl.setAttribute('style', 'font-size: 30px; position: relative; left: 400px');
+        body.appendChild(answerCommentEl);
+        renderQuestion2();
+    })};
+
+}
+
+/* correctOption = answerButton2;
+wrongOptions = [answerButton1, answerButton3, answerButton4];
+
+if (correctOption) {
+
+    correctOption.addEventListener('click', function () {
+        answerCommentEl.textContent = "Correct!";
+        answerCommentEl.setAttribute('style', 'font-size: 30px; position: relative; left: 400px');
+        body.appendChild(answerCommentEl);
+        renderQuestion3();
+    });
+} 
+if (wrongOptions) {
+       
+        for (var i = 0; i < wrongOptions.length; i++) {
+        wrongOptions[i].addEventListener('click', function () {
+        answerCommentEl.textContent = "Wrong ❌";
+        answerCommentEl.setAttribute('style', 'font-size: 30px; position: relative; left: 400px');
+        body.appendChild(answerCommentEl);
+        renderQuestion3();
+    })};
+
+} */
